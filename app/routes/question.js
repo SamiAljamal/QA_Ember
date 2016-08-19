0a3b63702aceb27907c63fb61bc741917ebc7991
sami al-jamal <sami.m.aljmal@gmail.com>
2016-08-19 15:18:51 -0700

    Add the destroyQuestion action to the question route.

--- app/routes/question.js
+++ app/routes/question.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params){
+    return this.store.findRecord('question',params.question_id)
+  },
+  actions:{
+    destroyQuestion(question){
+      var question_deletions = question.get('answers').map(function(rental){
+        return answer.destroyRecord();
+      });
+      Ember.RSVP.all(question_deletions).then(function(){
+        return question.destroyRecord();
+      });
+      this.transitionTo('index');
+    }
+  }
 });
