1dc3cc99bfcce43b1fbb7127a8900f37e7ed6bfc
sami Al-Jamal <Guest@epicodus-96.local>
2016-08-26 14:42:36 -0700

    Add helper to display a glyphicon to determine if a question is popular or not.

---
+++ app/helpers/question-popularity.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export function questionPopularity(params) {
+  var question = params[0];
+  if(question.get('answers').get('length') >5){
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"> Popular&nbsp;Question!</span>')
+  }
+  else if(question.get('answers').get('length')<1){
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-remove"> No&nbsp;Answer!</span>')
+  }
+
+}
+
+export default Ember.Helper.helper(questionPopularity);
