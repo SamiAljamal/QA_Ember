83ba55224b905b03eb9b1d4d1452b3b46a3c5a4b
sami Al-Jamal <Guest@epicodus-96.local>
2016-08-26 17:06:48 -0700

    Add service to create a list of favorite questions.

--- app/helpers/question-popularity.js
+++ app/helpers/question-popularity.js
@@ -3,10 +3,10 @@ import Ember from 'ember';
 export function questionPopularity(params) {
   var question = params[0];
   if(question.get('answers').get('length') >5){
-    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"> Popular&nbsp;Question!</span>')
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"> Popular&nbsp;Question!</span>');
   }
   else if(question.get('answers').get('length')<1){
-    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-remove"> No&nbsp;Answer!</span>')
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-remove"> No&nbsp;Answer!</span>');
   }
 
 }
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,17 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    return Ember.RSVP.hash({
-      questions: this.store.findAll('question'),
-      answers: this.store.findAll('answer')
-    });
-  },
+  favQ: Ember.inject.service(),
+
+  model(){
+   return this.store.findAll('question');
+ },
   actions: {
     saveQuestion(params){
       var newQuestion  = this.store.createRecord('question', params);
       newQuestion.save();
       this.transitionTo('index');
+    },
+    addtoFav(item){
+      this.get('favQ').add(item);
     }
   }
 
